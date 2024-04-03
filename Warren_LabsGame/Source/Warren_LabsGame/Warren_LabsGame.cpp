0ecd773f2081eae7df283af53abc6f79117bc326
Ian Warren <103790584+ManOfDuck@users.noreply.github.com>
2024-04-02 19:36:23 -0600

    Create LabsGame

---
+++ Warren_LabsGame/Source/Warren_LabsGame/Warren_LabsGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Warren_LabsGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Warren_LabsGame, "Warren_LabsGame" );
